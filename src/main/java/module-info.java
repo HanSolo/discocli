875805e1e91239475f4c7636898f25e6a7861672
Gerrit Grunwald <han.solo@mac.com>
2022-02-27 08:33:28 +0100

    Minor fix

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -9,6 +9,8 @@ module eu.hansolo.discocli {
     requires com.google.gson;
     requires info.picocli;
 
+    opens eu.hansolo.discocli to info.picocli;
+
     // Exports
     exports eu.hansolo.discocli.util;
     exports eu.hansolo.discocli;
